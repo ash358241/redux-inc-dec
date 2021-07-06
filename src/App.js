c6bac6f36625970cb21c263f95e805eb740a7eda
Ashraful Islam <ashrafunit7@gmail.com>
2021-07-07 03:34:17 +0600

    redux-increment-decrement

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import "./App.css";
+import {incNumber, decNumber} from './actions/index';
+
+const App = () => {  
+const myState = useSelector(state => state.changeTheNumber);
+const dispatch = useDispatch();
+  return (
+    <>
+      <div className="main-div">
+      
+      <div className="container">
+  
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+      
+      <div className="quantity">
+        <a href onClick={() => dispatch(decNumber())} className="quantity__minus" title="Decrement"><span>-</span></a>
+        <input name="quantity" type="text" className="quantity__input" value={myState} />
+        <a href onClick={() => dispatch(incNumber())} className="quantity__plus" title="Increment"><span>+</span></a>
+      </div>
+  
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default App
